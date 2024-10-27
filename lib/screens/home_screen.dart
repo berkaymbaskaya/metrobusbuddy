6a01a2698e22001792a6dad6614ce9a05615bbb2
berkay.baskaya <bbaskaya@payby.me>
2024-10-27 22:09:05 +0300

    feat(navBar):bottom navbar added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,8 +3,49 @@ import 'package:workspace/core/operation/station_operation.dart';
 import 'package:workspace/features/station.dart';
 import 'package:workspace/models/class/station.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: const Text('Home Screen'),
+  //     ),
+  //     body: Center(
+  //       child: StationWidget(
+  //         previousStation: DataOperation().getRandomStation(),
+  //         currentStation: DataOperation().getRandomStation(),
+  //         nextStation: DataOperation().getRandomStation(),
+  //       ),
+  //     ),
+  //   );
+  // }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  // Sekmelere göre gösterilecek sayfaları tanımlıyoruz
+  final List<Widget> _pages = <Widget>[
+    Center(
+      child: StationWidget(
+        previousStation: DataOperation().getRandomStation(),
+        currentStation: DataOperation().getRandomStation(),
+        nextStation: DataOperation().getRandomStation(),
+      ),
+    ),
+    Center(child: Text('Search Page', style: TextStyle(fontSize: 24))),
+    Center(child: Text('Profile Page', style: TextStyle(fontSize: 24))),
+  ];
+
+  // Sekme seçildiğinde sayfayı değiştirmek için kullanılan fonksiyon
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,12 +53,25 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Screen'),
       ),
-      body: Center(
-        child: StationWidget(
-          previousStation: DataOperation().getRandomStation(),
-          currentStation: DataOperation().getRandomStation(),
-          nextStation: DataOperation().getRandomStation(),
-        ),
+      body: _pages[_selectedIndex], // Seçilen sayfayı göstermek için
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
       ),
     );
   }
